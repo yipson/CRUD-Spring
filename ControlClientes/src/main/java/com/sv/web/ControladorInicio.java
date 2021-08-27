aa4f2193eb09393a439a63eed437ae41aa9d3923
Sebastian Valencia <sebastianvalencia.ceron@gmail.com>
2021-08-27 11:24:53 -0500

    CRUD Spring

---
+++ ControlClientes/src/main/java/com/sv/web/ControladorInicio.java
@@ -0,0 +1,55 @@
+package com.sv.web;
+
+import com.sv.domain.Persona;
+import com.sv.servicio.PersonaService;
+import java.util.ArrayList;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+
+@Controller
+@Slf4j
+public class ControladorInicio {
+    
+    @Autowired
+    private PersonaService personaService;
+    
+    @GetMapping("/")
+    public String inicio(Model model){
+
+        ArrayList<Persona> personas = (ArrayList) personaService.listarPersonas();
+        
+        log.info("ejecutando el controlador Spring MVC");
+        model.addAttribute("personas", personas);
+        
+        return "index";
+    }
+    
+    @GetMapping("/agregar")
+    public String agregar(Persona persona){
+        return "modificar";
+    }
+    
+    @PostMapping("/guardar")
+    public String guardar(Persona persona){
+        personaService.guardar(persona);
+        return "redirect:/";
+    }
+    
+    @GetMapping("/editar/{idPersona}")
+    public String editar(Persona persona, Model model){
+        persona = personaService.encontrarPersona(persona);
+        model.addAttribute("persona", persona);
+        return "modificar";
+    }
+    
+    @GetMapping("/eliminar") // (Manera #2)
+//    @GetMapping("/eliminar/{idPersona}") //(Manera #1)
+    public String eliminar(Persona persona){
+        personaService.eliminar(persona);
+        return "redirect:/";
+    }
+}
